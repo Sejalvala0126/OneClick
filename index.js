aa34d5fe681123ef83630a0b5fc49718f043badb
Ravibhola0126 <sejalvala179@gmail.com>
2023-09-05 09:28:23 +0530

    change

---
+++ index.js
@@ -0,0 +1,56 @@
+function first()
+{
+    document.getElementById("ok").style.backgroundColor="red";
+    document.getElementById("p").style.backgroundColor="grey";
+
+
+
+
+}
+function second()
+{
+    document.getElementById("ok").style.backgroundColor="orange";
+    document.getElementById("p").style.backgroundColor="olive";
+   
+
+
+}
+
+function third()
+{
+    document.getElementById("ok").style.backgroundColor="yellow";
+    document.getElementById("p").style.backgroundColor="hotpink";
+
+
+}
+
+function four()
+{
+    document.getElementById("ok").style.backgroundColor="green";
+    document.getElementById("p").style.backgroundColor="skyblue";
+
+
+}
+
+function five()
+{
+    document.getElementById("ok").style.backgroundColor="blue";
+    document.getElementById("p").style.backgroundColor="darkseagreen";
+
+
+}
+function six()
+{
+    document.getElementById("ok").style.backgroundColor="indigo";
+    document.getElementById("p").style.backgroundColor="gold";
+
+
+}
+
+function seven()
+{
+    document.getElementById("ok").style.backgroundColor="violet";
+    document.getElementById("p").style.backgroundColor="tomato";
+
+
+}
